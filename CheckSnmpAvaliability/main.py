344384c127e4361786c6ef7612e396df66ee2988
Traveller <51471579+TravellerXi@users.noreply.github.com>
2020-09-01 16:00:52 +0800

    Create main.py

---
+++ CheckSnmpAvaliability/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding:utf-8
+from pyzabbix import ZabbixAPI
+import warnings
+
+
+if __name__ == '__main__':
+    ZABBIX_SERVER = 'https://serveraddress.com'
+    zapi = ZabbixAPI(ZABBIX_SERVER)
+    warnings.filterwarnings('ignore')
+    # Disable SSL certificate verification
+    zapi.session.verify = False
+    zapi.login("username", "password")
+    SnmpUnreachable=zapi.host.get(snmp_available=2)
+    hostnames=[]
+    for host in SnmpUnreachable:
+        hostnames.append(host['host'])
+    with open ('Snmp不可达主机列表.txt','w') as f:
+        for hostname in hostnames:
+            f.write(hostname+'\n')
+    print('任务完毕')
