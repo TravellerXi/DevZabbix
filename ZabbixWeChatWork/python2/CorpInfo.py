35f7bd1d5d2994412292ce44c5ba417b56866481
Traveller <51471579+TravellerXi@users.noreply.github.com>
2020-09-19 00:37:56 +0800

    Add files via upload

---
+++ ZabbixWeChatWork/python2/CorpInfo.py
@@ -0,0 +1,12 @@
+#企业微信相关信息，需要在企业微信里开通一个应用
+sToken = ''
+sEncodingAESKey = 'W'
+sCorpID =''
+CorpID=sCorpID
+CorpSecret=''
+AgentID=1
+#部署环境有代理，请填写代理信息
+proxy = {'http': 'http://127.0.0.1:1080', 'https': 'http://127.0.0.1:1080'}
+#以下是zabbix用户信息
+username=''
+password=''
